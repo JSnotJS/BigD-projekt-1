e3ecee36a091a6b5b232a4abc4d0213f65f443b6
JSnotJS <jakubstefanski2002@interia.pl>
2024-10-21 19:57:20 +0200

    nie działa ale wygląda ładnie

--- src/main/java/com/bd151876/project1/MapperKey.java
+++ src/main/java/com/bd151876/project1/MapperKey.java
@@ -12,16 +12,25 @@ import java.io.IOException;
 public class MapperKey implements WritableComparable<MapperKey> {
     Text street;
     Text zip_code;
+    Text victim_type;
+    Text dmg_type;
 
     public MapperKey() {
-        set(new Text(), new Text());
+        set(new Text(), new Text(), new Text(), new Text());
     }
-    public MapperKey(String street, String zip_code) {
-        set(new Text(street), new Text(zip_code));
+    public MapperKey(String street, String zip_code, String victim_type, String dmg_type) {
+        set(
+            new Text(street),
+            new Text(zip_code),
+            new Text(victim_type),
+            new Text(dmg_type)
+        );
     }
-    public void set(Text street, Text zip_code) {
+    public void set(Text street, Text zip_code, Text victim_type, Text dmg_type) {
         this.street = street;
         this.zip_code = zip_code;
+        this.victim_type = victim_type;
+        this.dmg_type = dmg_type;
     }
     public Text getStreet() {
         return street;
@@ -29,16 +38,22 @@ public class MapperKey implements WritableComparable<MapperKey> {
     public Text getZipCode() {
         return zip_code;
     }
+    public Text getVictimType() { return victim_type; }
+    public Text getDmgType() { return dmg_type; }
     @Override
     public void write(DataOutput dataOutput) throws IOException {
         street.write(dataOutput);
         zip_code.write(dataOutput);
+        victim_type.write(dataOutput);
+        dmg_type.write(dataOutput);
     }
 
     @Override
     public void readFields(DataInput dataInput) throws IOException {
         street.readFields(dataInput);
         zip_code.readFields(dataInput);
+        victim_type.readFields(dataInput);
+        dmg_type.readFields(dataInput);
     }
 
     @Override
--- src/main/java/com/bd151876/project1/NycAccidents.java
+++ src/main/java/com/bd151876/project1/NycAccidents.java
@@ -1,9 +1,6 @@
 package com.bd151876.project1;
-import com.example.bigdata.SumCount;
-import org.apache.commons.math3.optim.linear.LinearObjectiveFunction;
 import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.DoubleWritable;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.LongWritable;
 import org.apache.hadoop.io.Text;
@@ -14,7 +11,9 @@ import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 import org.apache.hadoop.util.Tool;
 import org.apache.hadoop.util.ToolRunner;
+import org.codehaus.jackson.map.MapperConfig;
 
+import java.io.Console;
 import java.io.IOException;
 import java.util.Objects;
 
@@ -25,184 +24,192 @@ public class NycAccidents extends Configured implements Tool {
     }
 
     public int run(String[] args) throws Exception {
-        Job job = Job.getInstance(getConf(), "AvgSizeStations");
+        Job job = Job.getInstance(getConf(), "NycAccidents");
         job.setJarByClass(this.getClass());
 
         FileInputFormat.addInputPath(job, new Path(args[0]));
         FileOutputFormat.setOutputPath(job, new Path(args[1]));
 
         job.setMapperClass(NycAccidentsMapper.class);
-//        job.setCombinerClass(NycAccidents.AvgSizeStationCombiner.class);
+        job.setCombinerClass(NycAccidentsCombiner.class);
         job.setReducerClass(NycAccidentsReducer.class);
 
         job.setMapOutputKeyClass(MapperKey.class);
-        job.setMapOutputValueClass(MapperVal.class);
+        job.setMapOutputValueClass(IntWritable.class);
 
         job.setOutputKeyClass(Text.class);
         job.setOutputValueClass(Text.class);
         return job.waitForCompletion(true) ? 0 : 1;
     }
 
-    public static class NycAccidentsMapper extends Mapper<LongWritable, Text, MapperKey, MapperVal> {
-        private final MapperKey key1 = new MapperKey();
-        private final MapperKey key2 = new MapperKey();
-        private final MapperKey key3 = new MapperKey();
-        private final MapperVal value = new MapperVal();
+    public static class NycAccidentsMapper extends Mapper<LongWritable, Text, MapperKey, IntWritable> {
         Boolean[] streets_to_send = {Boolean.FALSE, Boolean.FALSE, Boolean.FALSE};
         String zipCode;
-        String onStreetName;
-        String crossStreetName;
-        String offStreetName;
-        int numberOfPersonsInjured;
-        int numberOfPersonsKilled;
+        String[] streets = {"", "", ""};
         int numberOfPedestriansInjured;
         int numberOfPedestriansKilled;
         int numberOfCyclistsInjured;
         int numberOfCyclistsKilled;
         int numberOfMotoristsInjured;
         int numberOfMotoristsKilled;
-        int totalKilledInjured = 0; // liczba poszkodowanych
-        int pedestrians = 0; // typ poszkodowanych
-        int cyclist = 0; // typ poszkodowanych
-        int motorist = 0; // typ poszkodowanych
-        int killed = 0; // charakter obrażeń
-        int injured = 0; // charakter obrażeń
         public void map(LongWritable offset, Text lineText, Context context) {
             try {
-                if (offset.get() != 0) {
-                    String line = lineText.toString();
-                    int i = 0;
 
-                    for (String word : line.split(",")) {
-                        if (i == 0) {
-                            if (Integer.parseInt(word.split("/")[2]) < 2012) {return;}
-                        } // data
-                        if (i == 2) {
-                            zipCode = word;
-                            if (Objects.equals(word, "")) {return;}
-                        } // zip_code
-                        if (i == 6) {
-                            onStreetName = word;
-                            key1.set(new Text(onStreetName), new Text(zipCode));
+                String line = lineText.toString();
+                int i = 0;
+                for (String word : line.split(",")) {
+                    if (i==0) {
+                        if (Integer.parseInt(word.split("/")[2]) < 2012) { return; }
+                        break;
+                    } // data późniejsza niż 2012
+                    if (i==2) {
+                        zipCode = word;
+                        if (Objects.equals(word, "")) { return; }
+                        break;
+                    } // zip_code + sprawdzenie czy istnieje
+                    if (i==6) {
+                        if (!Objects.equals(word, "")) {
+                            streets[0] = word;
                             streets_to_send[0] = Boolean.TRUE;
-                        } // on_street_name
-                        if (i == 7) {
-                            crossStreetName = word;
-                            key2.set(new Text(onStreetName), new Text(zipCode));
+                        }
+                    } // on_street_name
+                    if (i==7) {
+                        if (!Objects.equals(word, "")) {
+                            streets[1] = word;
                             streets_to_send[1] = Boolean.TRUE;
-                        } // cross_street_name
-                        if (i == 8) {
-                            offStreetName = word;
-                            key3.set(new Text(onStreetName), new Text(zipCode));
+                        }
+                    } // cross_street_name
+                    if (i==8) {
+                        if (!Objects.equals(word, "")) {
+                            streets[2] = word;
                             streets_to_send[2] = Boolean.TRUE;
-                        } // off_street_name
-                        if (i == 9) {
-                            numberOfPersonsInjured = Integer.parseInt(word);
-
-                        } // numer_of_persons_injured
-                        if (i == 10) {
-                            numberOfPersonsKilled = Integer.parseInt(word);
-
-                        } // numer_of_persons_killed
-                        if (i == 11) {
-                            numberOfPedestriansInjured = Integer.parseInt(word);
-                            pedestrians += numberOfPedestriansInjured;
-                        } // numer_of_pedestrians_injured
-                        if (i == 12) {
-                            numberOfPedestriansKilled = Integer.parseInt(word);
-                            pedestrians += numberOfPedestriansKilled;
-                        } // numer_of_pedestrians_killed
-                        if (i == 13) {
-                            numberOfCyclistsInjured = Integer.parseInt(word);
-                            cyclist += numberOfCyclistsInjured;
-                        } // numer_of_cyclist_injured
-                        if (i == 14) {
-                            numberOfCyclistsKilled = Integer.parseInt(word);
-                            cyclist += numberOfCyclistsKilled;
-                        } // numer_of_cyclist_killed
-                        if (i == 15) {
-                            numberOfMotoristsInjured = Integer.parseInt(word);
-                            motorist += numberOfMotoristsInjured;
-                        } // numer_of_motorist_injured
-                        if (i == 16) {
-                            numberOfMotoristsKilled = Integer.parseInt(word);
-                            motorist += numberOfMotoristsKilled;
-                        } // numer_of_motorist_killed
-                        i++;
-                    }
-                    value.set(
-                        new IntWritable(pedestrians),
-                        new IntWritable(cyclist),
-                        new IntWritable(motorist),
-                        new IntWritable(numberOfPersonsInjured),
-                        new IntWritable(numberOfPersonsKilled)
-                    );
+                        }
+                    } // off_street_name
+                    if (i==11) {
+                        numberOfPedestriansInjured = Integer.parseInt(word);
+                        if (numberOfPedestriansInjured > 0) {
+                            this.printuj(numberOfMotoristsKilled, "injured", "pedestrians", context);
+//                                for (int j = 0; j < 3; j++) {
+//                                    if (streets_to_send[j].equals(Boolean.TRUE)) {
+//                                        context.write(
+//                                            new MapperKey(zipCode, streets[j], "injured", "pedestrians"),
+//                                            new IntWritable(numberOfPedestriansInjured));
+//                                    }
+//                                }
+                        }
+                    } // numer_of_pedestrians_injured
+                    if (i==12) {
+                        numberOfPedestriansKilled = Integer.parseInt(word);
+                        if (numberOfPedestriansKilled > 0) {
+                            this.printuj(numberOfMotoristsKilled, "killed", "pedestrians", context);
+//                                for (int j = 0; j < 3; j++) {
+//                                    if (streets_to_send[j].equals(Boolean.TRUE)) {
+//                                        context.write(
+//                                            new MapperKey(zipCode, streets[j], "killed", "pedestrians"),
+//                                            new IntWritable(numberOfPedestriansKilled));
+//                                    }
+//                                }
+                        }
+                    } // numer_of_pedestrians_killed
+                    if (i==13) {
+                        numberOfCyclistsInjured = Integer.parseInt(word);
+                        if (numberOfCyclistsInjured > 0) {
+                            this.printuj(numberOfMotoristsKilled, "injured", "cyclists", context);
+//                                for (int j = 0; j < 3; j++) {
+//                                    if (streets_to_send[j].equals(Boolean.TRUE)) {
+//                                        context.write(
+//                                            new MapperKey(zipCode, streets[j], "injured", "cyclists"),
+//                                            new IntWritable(numberOfCyclistsInjured));
+//                                    }
+//                                }
+                        }
+                    } // numer_of_cyclist_injured
+                    if (i==14) {
+                        numberOfCyclistsKilled = Integer.parseInt(word);
+                        if (numberOfCyclistsKilled > 0) {
+                            this.printuj(numberOfMotoristsKilled, "killed", "cyclists", context);
+//                                for (int j = 0; j < 3; j++) {
+//                                    if (streets_to_send[j].equals(Boolean.TRUE)) {
+//                                        context.write(
+//                                            new MapperKey(zipCode, streets[j], "killed", "cyclists"),
+//                                            new IntWritable(numberOfCyclistsKilled));
+//                                    }
+//                                }
+                        }
+                    } // numer_of_cyclist_killed
+                    if (i==15) {
+                        numberOfMotoristsInjured = Integer.parseInt(word);
+                        if (numberOfMotoristsInjured > 0) {
+                            this.printuj(numberOfMotoristsKilled, "injured", "motorists", context);
+//                                for (int j = 0; j < 3; j++) {
+//                                    if (streets_to_send[j].equals(Boolean.TRUE)) {
+//                                        context.write(
+//                                            new MapperKey(zipCode, streets[j], "injured", "motorists"),
+//                                            new IntWritable(numberOfMotoristsInjured));
+//                                    }
+//                                }
+                        }
+                    } // numer_of_motorist_injured
+                    if (i==16) {
+                        numberOfMotoristsKilled = Integer.parseInt(word);
+                        if (numberOfMotoristsKilled > 0) {
+                            this.printuj(numberOfMotoristsKilled, "killed", "motorists", context);
+//                                for (int j = 0; j < 3; j++) {
+//                                    if (streets_to_send[j].equals(Boolean.TRUE)) {
+//                                        context.write(
+//                                            new MapperKey(zipCode, streets[j], "killed", "motorists"),
+//                                            new IntWritable(numberOfMotoristsKilled));
+//                                    }
+//                                }
+                        }
+                    } // numer_of_motorist_killed
+                    i += 1;
                 }
-                if (streets_to_send[0]) { context.write(key1, value); }
-                if (streets_to_send[1]) { context.write(key2, value); }
-                if (streets_to_send[2]) { context.write(key3, value); }
-            } catch ( Exception e) {
+//                    context.write(new MapperKey("siema","eniu","zlamane","kolano"), new IntWritable(1));
+
+            } catch ( Exception e ) {
                 e.printStackTrace();
+            }
+        }
+
+        private void printuj(int val, String dmg, String person, Context cont) throws IOException, InterruptedException {
+            for (int j = 0; j < 3; j++) {
+                if (streets_to_send[j].equals(Boolean.TRUE)) {
+                    cont.write(
+                            new MapperKey(zipCode, streets[j], "killed", "motorists"),
+                            new IntWritable(numberOfMotoristsKilled));
+                }
             }
         }
     }
 
 
-    public static class NycAccidentsReducer extends Reducer<MapperKey, MapperVal, Text, Text> {
+    public static class NycAccidentsReducer extends Reducer<MapperKey, IntWritable, Text, Text> {
         int total;
-        int pedestrians;
-        int cyclist;
-        int motorist;
-        int killed;
-        int injured;
 
         @Override
-        public void reduce(MapperKey key, Iterable<MapperVal> values, Context context) throws IOException, InterruptedException {
+        public void reduce(MapperKey key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
             total = 0;
-            pedestrians = 0;
-            cyclist = 0;
-            motorist = 0;
-            killed = 0;
-            injured = 0;
-//            Text resultKey = new Text("stats for  " + key.getStreet() + " street in '" + key.getZipCode() + "' post area was: ");
-            Text resultKey = new Text(key.getStreet() + " " + key.getZipCode() + " ");
-            for (MapperVal val : values) {
-                pedestrians += val.getPedestrians().get();
-                cyclist += val.getCyclist().get();
-                motorist += val.getMotorist().get();
-                killed += val.getKilled().get();
-                injured += val.getInjured().get();
-                total += killed + injured;
+            Text resultKey = new Text(key.getStreet() + " " + key.getZipCode() + " " + key.getVictimType() + " " + key.getDmgType() + " ");
+            for (IntWritable val : values) {
+                total += val.get();
             }
-            Text result = new Text(
-                String.join(" ", new String[] {
-                        String.valueOf(pedestrians),
-                        String.valueOf(cyclist),
-                        String.valueOf(motorist),
-                        String.valueOf(killed),
-                        String.valueOf(injured),
-                        String.valueOf(total)
-                    }
-                )
-            );
+            Text result = new Text(String.valueOf(total));
             context.write(resultKey, result);
 
         }
     }
 
-    public static class NycAccidentsCombiner extends Reducer<MapperKey, MapperVal, MapperKey, SumCount> {
-
-        private final MapperVal sum = new MapperVal();
+    public static class NycAccidentsCombiner extends Reducer<MapperKey, IntWritable, MapperKey, IntWritable> {
+        int sum;
 
         @Override
-        public void reduce(MapperKey key, Iterable<MapperVal> values, Context context) throws IOException, InterruptedException {
-
-//            sum.set(new DoubleWritable(0.0d), new IntWritable(0));
-
-            for (MapperVal val : values) {
-                sum.add(val);
+        public void reduce(MapperKey key, Iterable<IntWritable> values, Context context) throws IOException, InterruptedException {
+            sum = 0;
+            for (IntWritable val : values) {
+                sum += val.get();
             }
-//            context.write(key, sum);
+            context.write(key, new IntWritable(sum));
         }
     }
 }
